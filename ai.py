5bcf5c6f63ed66eaa3cec14501b7edba643a1f0f
Stanleyjameson <stanleyjameson111@gmail.com>
2021-01-14 22:28:04 +0200

    implemented a basic function

--- ai.py
+++ ai.py
@@ -1,3 +1,20 @@
 import streamlit as st
 import numpy as np
 import pandas as p
+import matplotlib.pyplot as plt
+import seaborn as sns
+import folium
+
+def calculateinterest(salary,rate):
+    interest = salary* rate
+
+    print("The interest is %d :" % interest )
+
+
+
+
+calculateinterest(44,10)
+
+
+
+
